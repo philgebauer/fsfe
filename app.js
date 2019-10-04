4b86290a315e7ed672b8907abe135740180386ac
philgebauer <gebauer.phil@gmail.com>
2019-10-04 19:27:45 +0000

    updated express route

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hellllllo World!'))
+
+app.get('/demo', (req,res) => {
+    res.set('X-full-stack', '4life');
+    res.status(418);
+    res.send('I prefer coffee');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
